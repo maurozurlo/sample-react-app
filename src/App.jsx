5cfdddecb228b6eae6d9a1bba0dcb39e98f47bc2
Mauro Zurlo <mauro.e.zurlo@gmail.com>
2025-02-04 14:06:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+
+const isMaintenanceMode = import.meta.env.VITE_MAINTENANCE_MODE === 'true';
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isMaintenanceMode ? <Maintenance /> : <Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h1>Hello, World!</h1>;
+}
+
+function Maintenance() {
+  return <h1>Sorry, we're currently under scheduled maintenance.</h1>;
+}
+
+export default App;
